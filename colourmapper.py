8a42ea272d9498bba0cacbd044e1992d0a990f65
rmdocherty <rmdocherty2@gmail.com>
2021-07-29 12:51:47 +0100

    Adjusted colour_map to stack LCPL and RCPL images side by side with np.hstack but note this only works if they're the same size image (which they may not be if we have to crop at all / use ROI). Might need some padding if this is to be permanent but that might be computationally costly in terms of real time viewing.

--- colourmapper.py
+++ colourmapper.py
@@ -8,6 +8,7 @@ Created on Thu Jul 29 11:35:13 2021
 
 from matplotlib import cm
 from PIL import Image
+import numpy as np # maybe can just import hstack by itself later
 
 
 class ColourMapper():
@@ -53,13 +54,13 @@ class ColourMapper():
                 cmap_dict[temp_mode] = temp_cmap
         return cmap_dict
 
-    def colour_map(self, img1, img2, mode):
+    def _colour_map(self, img1, img2, mode, debug=False):
         """
         colour_map.
 
         Given the two images from the camera and the mode, perform the mapping
-        process. If in Raw mode, cmap both images separately and return tuple
-        of images. If in g_em mode, combine both arrays using g_em formula
+        process. If in Raw mode, cmap both images separately, combine and then
+        return image. If in g_em mode, combine both arrays using g_em formula
         before cmapping and returning single image. If in DOCP mode, calc. the
         DOCP by adding magnitudes then cmap and return single image.
 
@@ -76,26 +77,31 @@ class ColourMapper():
 
         Returns
         -------
-        Either:
-            1) In raw mode, a TUPLE of PIL IMAGES
-            2) A single PIL IMAGE
+            A PIL Image Object
 
         """
         if mode == "Raw":
             cmap1, cmap2 = self._cmaps["Raw"] # unpack
             cmapped_img1 = self._single_cmap(img1, cmap1)
             cmapped_img2 = self._single_cmap(img2, cmap2)
-            out = [cmapped_img1, cmapped_img2]
+            mapped = np.hstack((cmapped_img1, cmapped_img2)) # put images 'side-by-side' NB - will this work if diff sizes??
+
         elif mode == "g_em":
             cmap = self._cmaps["g_em"]
-            g_em = 2 * (img1 - img2) / (img1 + img2)
-            out = self._single_cmap(g_em, cmap)
+            g_em = 2 * (img1 - img2) / (img1 + img2) # from equation
+            mapped = self._single_cmap(g_em, cmap)
+
         elif mode == "DOCP":
             cmap = self._cmaps["DOCP"]
             DOCP = (img1 + img2)
-            out = self._single_cmap(DOCP, cmap)
+            mapped = self._single_cmap(DOCP, cmap)
+
         else:
             raise Exception("Invalid mode supplied, check cmap_config file")
+
+        out = Image.fromarray(mapped) # convert to PIL Image
+        if debug is True:
+            out.show() #see what's going on
         return out
 
     def _single_cmap(self, img, cmap_string):
@@ -117,9 +123,18 @@ class ColourMapper():
             Colour mapped PIL image for use in the tkinter widget.
         """
         cmap = cm.get_cmap(cmap_string) # grab cmap object from string
-        out_im = Image.fromarray(cmap(img, bytes=True)) # bytes=True converts array into uint8 for fromarray to use
+        out_im = cmap(img, bytes=True) # bytes=True converts array into uint8 for fromarray to use
         return out_im
 
 
+# testing stuff here
+np.random.seed(1)
+
+IMG_WIDTH = 200
+IMG_HEIGHT = 200
+
+LCPL = np.random.random((IMG_HEIGHT, IMG_WIDTH))
+RCPL = np.random.random((IMG_HEIGHT, IMG_WIDTH))
 
 c = ColourMapper("test")
+c._colour_map(LCPL, RCPL, "Raw", debug=True)
