85787940ba968325a7f561aada80fb028f69c987
rmdocherty <rmdocherty2@gmail.com>
2021-08-06 15:40:51 +0100

    Added placholder rotator code - should work with ELL14 provided don't need to do any more setup.

--- rotator.py
+++ rotator.py
@@ -7,3 +7,43 @@ Created on Fri Aug  6 14:33:00 2021
 """
 
 import serial
+
+PULSES_PER_DEGREE = 262144 / 360
+
+
+class Rotator():
+    def __init__(self, port): #might need to add flushing and setting params
+        try:
+            self._port = serial.Serial(port) #can set baud rate here, want 9600!
+        except Exception as error:
+            print(f"Error: {error}, please supply a port.")
+
+    def _get_set_angle_string(self, angle):
+        pulses = int(angle * PULSES_PER_DEGREE) #should i use floor or ceil?
+        hex_string = hex(pulses)
+        return hex_string
+
+    def _send_set_angle_string(self, ang_str):
+        command = "Ama" + ang_str
+        self._port.write(command)
+
+    def _send_get_status(self):
+        self._port.write("Ags")
+
+    def _check_angle(self, ang_str): #should block wwhile rotating
+        rotated = False
+        while rotated is False: #block until device says has rotated to specified angle
+            self._send_get_status()
+            data = self._port.read(11)
+            position_command = "APO" + ang_str
+            if data == position_command: #might be able to just set if data[:3] == "APO" if this doesn't work
+                rotated = True
+                return 0
+            else:
+                pass
+
+    def rotate_to_angle(self, angle):
+        set_angle_str = self._get_set_angle_string(angle)
+        self._send_set_angle_string(set_angle_str)
+        self._check_angle(set_angle_str)
+        return 0
